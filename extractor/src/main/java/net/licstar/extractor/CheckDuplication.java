875138171ed4c3d00bd99319464815199ce805fc
licstar <licstar@sina.com>
2016-03-20 12:25:28 +0800

    simhash check

--- extractor/src/main/java/net/licstar/extractor/CheckDuplication.java
+++ extractor/src/main/java/net/licstar/extractor/CheckDuplication.java
@@ -14,29 +14,29 @@ public class CheckDuplication {
 
 
     public static void main(String[] args) {
-        if(args.length != 2){
+        if (args.length != 2) {
             System.out.println("usage: java -jar CheckDuplication input_file index_file");
             return;
         }
         try {
-            String md5 = FileHash.getMD5(args[0]);
+            String md5 = FileHash.getSimHash(args[0]);
             List<String> result = new ArrayList<String>();
 
             FileInputStream fis = new FileInputStream(args[1]);
             InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
-            Scanner sc=new Scanner(isr);
-            while(sc.hasNext()){
+            Scanner sc = new Scanner(isr);
+            while (sc.hasNext()) {
                 String line = sc.nextLine();
                 String[] part = line.split(" ");
-                if(part[1].equals(md5)){
+                if (part[1].equals(md5)) {
                     result.add(part[0]);
                 }
             }
 
-            if(result.size() == 0){
+            if (result.size() == 0) {
                 System.out.println("cannot find duplication");
-            }else{
-                for (String s: result) {
+            } else {
+                for (String s : result) {
                     System.out.println(s);
                 }
             }
